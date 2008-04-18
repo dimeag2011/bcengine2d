ac49e8d1c6282165df559237e94dda61b7fcd623
brenner.daniel@gmail.com <brenner.daniel@gmail.com@d8b39de5-654a-0410-bfe5-fd9c3412c68c>
2008-04-18 22:54:55 +0000

    * Actualizado shape, ahora se puede dibujar triangulo o caja

--- trunk/src/TestGame/TestGame.cpp
+++ trunk/src/TestGame/TestGame.cpp
@@ -15,9 +15,10 @@ TestGame::~TestGame ()
 //----------------------------------------------------------------
 bool TestGame::OnStartUp ()
 {
-	m_pkShape = new Shape();
+	m_pkShape = new Shape(m_pkShape->TRIANGLE);
 	m_pkShape->setDim(100,100);
-	m_pkShape->setPos(0,0);
+	m_pkShape->setPos(50,50);
+	m_pkShape->setRotation(45);
 
 	addEntity(m_pkShape);
 
