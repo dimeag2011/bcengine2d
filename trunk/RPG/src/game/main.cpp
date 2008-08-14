15e9fac1fc466df84b31cdba3962f562a8fef47d
brenner.daniel <brenner.daniel@d8b39de5-654a-0410-bfe5-fd9c3412c68c>
2008-08-14 22:41:55 +0000

    ! Renombro los archivos, esqueleto vacio de RPG para DJ3 LISTO!!!!

--- trunk/RPG/src/game/main.cpp
+++ trunk/RPG/src/game/main.cpp
@@ -1,7 +1,7 @@
 
 //--------------------------------------------------------------------------------
 #include <Windows.h>
-#include "BasicGame.h"
+#include "RPG.h"
 //--------------------------------------------------------------------------------
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 				   LPSTR lpCmdLine,int intnShowCmd)
