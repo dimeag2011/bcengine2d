901d534212af9673b44e3126ac521199c64801dd
brenner.daniel <brenner.daniel@d8b39de5-654a-0410-bfe5-fd9c3412c68c>
2008-08-19 22:18:22 +0000

    + Clase Component arreglada, implementada la funcion recursiva para probar components.

--- trunk/RPG/test/component/component/main.cpp
+++ trunk/RPG/test/component/component/main.cpp
@@ -6,32 +6,54 @@
 
 using namespace std;
 
+void printComponent(Component* pkComponent);
+
 void main()
 {
 	Component* pkMochila = new Component();
 	pkMochila->setName("Mochila");
 	pkMochila->setType("container");
-	cout << "Nombre: " << pkMochila->getName() << " - Type: " << pkMochila->getType() << endl;
+
 	Component* pkPot = new Component();
 	pkPot->setName("Pot");
 	pkPot->setType("consumible");
+
 	Component* pkPot2 = new Component("Pot2", "consumible2");
+
 	pkMochila->addNewChild(pkPot);
 	pkMochila->addNewChild(pkPot2);
-	Component* pkAux = pkMochila->getCurrentChild();
-	pkMochila->goFirstChild();
-	pkAux = pkMochila->getCurrentChild();
-	cout << "Nombre: " << pkAux->getName() << " - Type: " << pkAux->getType() << " - Parent Name: "  << pkAux->getParent()->getName() << endl;
-	pkMochila->goNextChild();
-	pkAux = pkMochila->getCurrentChild();
-	pkPot->setNewParent(pkPot2);
-	pkMochila->removeChild(pkPot);
-	pkPot2->removeParent();
-	cout << "Nombre: " << pkAux->getName() << " - Type: " << pkAux->getType() << " - Parent Name: "  << pkAux->getParent()->getName() << endl;
+
+
+	//pkPot->setNewParent(pkPot2);
+	//pkMochila->addNewChild(pkPot);
+	//pkPot->addNewChild(pkPot2);
+	//pkMochila->removeChild(pkPot);
+	//pkPot2->removeParent();
+
+	printComponent(pkMochila);
+
 	system("PAUSE");
 }
 
-void printChilds(Component* pkRoot)
+void printComponent(Component* pkComponent)
 {
-
+	cout << "Nombre: " << pkComponent->getName() << endl;
+	cout << "Type: " << pkComponent->getType() << endl;
+	cout << "Padre: ";
+	if (pkComponent->getParent())
+	{
+		cout << pkComponent->getParent()->getName();
+	}
+	else
+	{
+		cout << "-";
+	}
+	cout << endl << "----------------------------------------------------" << endl;
+	int iSize = pkComponent->getNumChilds();
+	pkComponent->goFirstChild();
+	for(int i = 0; i < iSize; i++)
+	{
+		printComponent(pkComponent->getCurrentChild());
+		pkComponent->goNextChild();
+	}
 }
