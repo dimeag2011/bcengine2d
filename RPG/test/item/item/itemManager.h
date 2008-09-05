528d13c46d9c8e4ca76ff34c1de71d22b9d203c8
yoviacthulhu <yoviacthulhu@d8b39de5-654a-0410-bfe5-fd9c3412c68c>
2008-09-05 01:14:40 +0000

    Se agrego la clase ItemManager y class.h para la clase del actor. Se modifico item.h para que cree un objeto Skill y atributo, que falta definirlo en el item.cpp. Falta crear la clase actor, la potion y equipables.

---
+++ RPG/test/item/item/itemManager.h
@@ -0,0 +1,26 @@
+//-----------------------------------------------------------------------------------------
+#ifndef ITEMMNGR_H
+#define ITEMMNGR_H
+//-----------------------------------------------------------------------------------------
+#include "item.h"
+#include "armorWeapon.h"
+#include <list>
+#include "cstlib.h"
+//-----------------------------------------------------------------------------------------
+class ItemManager{
+//-----------------------------------------------------------------------------------------
+public:
+
+	ItemManager();
+	~ItemManager();
+
+	Item* CreateItem(string &m_kType);
+	void DeleteItem(Item *kItem);
+//-----------------------------------------------------------------------------------------
+private:
+	list<Item*>m_lkItemList;
+	list<Item*>m_lkItemListIterator;
+	string m_kType;
+	Item *m_ikAuxItem;
+
+};
