d7d2faf2dffbe839ee9aa6dc7b10a0f4e6cba5cc
yoviacthulhu <yoviacthulhu@d8b39de5-654a-0410-bfe5-fd9c3412c68c>
2008-09-20 01:03:12 +0000

    + Singleton implemented ;););) xD

---
+++ trunk/RPG/test/item/item/Include.h
@@ -0,0 +1,7 @@
+//--------------------Includes para probar-------------------------------------
+#include "component.h"
+#include "Defines.h"
+#include "functions.h"
+#include "item.h"
+#include "slot.h"
+#include "itemFactory.h"
--- trunk/RPG/test/item/item/main.cpp
+++ trunk/RPG/test/item/item/main.cpp
@@ -2,12 +2,23 @@
 #include <iostream>
 #include <string>
 
-#include "item.h"
-#include "functions.h"
+//#include "item.h"
+//#include "functions.h"
+#include "include.h"
 
 using namespace std;
 
 void main()
 {
+	//Creo el el factory
+	ItemFactory * myFactory    = ItemFactory::GetInstance();
+	ItemFactory * myAuxFactory = myFactory -> GetInstance();
+    ItemFactory & ref = * ItemFactory::GetInstance();
+
+
+	myAuxFactory->CreateItem(5);
+
+
+
 	system("PAUSE");
 }
