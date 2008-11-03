09ed9e62a47bb2ed595ead598ed34cd31235176b
yoviacthulhu <yoviacthulhu@d8b39de5-654a-0410-bfe5-fd9c3412c68c>
2008-11-03 23:35:29 +0000

    Arreglos en el event

--- RPG/src/game/event.h
+++ RPG/src/game/event.h
@@ -1,13 +1,31 @@
 #pragma once
 //-----------------------------------------------------------------------------------------
 #include <string>
+#include "component.h"
 //-----------------------------------------------------------------------------------------
 using namespace std;
 //-----------------------------------------------------------------------------------------
 class Event {
 public:
 	Event(string name) { Name = name; }
+	Event();
+	~Event();
 //-----------------------------------------------------------------------------------------
-	string Name;
+void setEvent(string name){ m_kName = name; }
+string getEvent() { return m_kName; }
+
+void setDescrip(string descrip){ m_kDescrip = descrip; }
+string getDescrip() { return m_kDescrip; }
+//-------------Eventos----------------------------------------------------------------------
+void childAddendEvent(string name, Component *pkChild);
+bool childRemovedEvent(string name, Component *pkChild);
+void addedToParentEvent(string name, Component *pkParent);
+bool removedFromParentEvent(string name, Component *pkParent);
+//-----------------------------------------------------------------------------------------
+private:
+	string m_kName;
+	string m_kDescrip;
+
+	Component *m_kChild;
 };
 //-----------------------------------------------------------------------------------------
--- RPG/src/game/world.cpp
+++ RPG/src/game/world.cpp
@@ -1,8 +1,6 @@
 //-----------------------------------------------------------------------------------------
 #include "world.h"
 //-----------------------------------------------------------------------------------------
-
-//-----------------------------------------------------------------------------------------
 World::World(){
 
 }
@@ -12,30 +10,35 @@ World::~World(){
 	
 }
 //-----------------------------------------------------------------------------------------
-void World::addWorldComp(Component* pkComp){
-
+void World::addWorldComp(Component *pkComp)
+{
 	if(addNewChild(pkComp));
 }
 //-----------------------------------------------------------------------------------------
 void World::GetCreations()
-{/*
-	/*
-	//Ver como hacer para que muestre el contenido
-	WorldListIterator = WorldList.begin();
-	//if(count > 0)
-	//	{	
-	while(WorldListIterator != WorldList.end())
-	{
-		m_iAux = *WorldListIterator;
-		cout << "Nombre: " << m_iAux.getName() << endl;
-
-		WorldListIterator++;
-	}
-*/
+{
+	
 }
 //-----------------------------------------------------------------------------------------
 bool World::removeWorldComp(Component* pkComp){
 
+	/*WorldListIterator = WorldList.begin();
+
+	while((WorldListIterator!= WorldList.end()) && (*WorldListIterator != pkComp))
+	{
+		WorldListIterator++;
+	}
+	if(WorldListIterator == WorldList.end()){
+
+		return false;
+
+	}else{
+
+		WorldList.erase(WorldListIterator);
+
+		return true;
+	}
+*/
 	return true;
 }
 //-----------------------------------------------------------------------
